782424cbf7ed8a9830e11f89c5c6e7a40e69d8fd
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2013-08-04 16:33:50 +1200

    XYZLayer: Example working with possum layer.

--- examples/xyz-tiles.js
+++ examples/xyz-tiles.js
@@ -6,27 +6,39 @@ goog.require('ol.projection');
 goog.require('ol.source.DebugTileSource');
 goog.require('ol.source.OSM');
 goog.require('ol.tilegrid.XYZ');
+goog.require('ol.source.ImageTileSource');
 
 
+function pad(n) {
+    var len = (""+n).length;
+    var s="";
+    for (var i = 3-len; i > 0; i--) {
+      s+="0";
+    }
+    return s+n;
+}
+function createUrl(x, y, z) {
+    var url = 'http://proxy.zen.landcareresearch.co.nz/possums/cache/possum_2009_cache_nztm/portalgridnztm1/';
+    var zeros = '000';
+    var s = '0'+ z +'/'+ zeros +'/'+ zeros +'/'+ pad(x) +'/'+ zeros +'/'+ zeros +'/'+ pad(y) +'.png';
+    return url + s;
+}
 var view = new ol.View2D({
     center: [-20000000,20001000],
-    zoom: 10
+    zoom: 0
   });
 var map = new ol.Map({
   layers: [
     new ol.layer.TileLayer({
-      source: new ol.source.OSM()
-    }),
-    new ol.layer.TileLayer({
-      source: new ol.source.DebugTileSource({
-        projection: 'EPSG:3857',
-        tileGrid: new ol.tilegrid.XYZ({
-          maxZoom: 22
-        })
+      source: new ol.source.ImageTileSource({
+        tileUrlFunction: function(opt) {
+          return createUrl(opt.x, opt.y, opt.z);
+        }
       })
     })
   ],
-  renderers: ol.RendererHints.createFromQueryData(),
+  renderer: ol.RendererHint.DOM,
+  //renderers: ol.RendererHints.createFromQueryData(),
   target: 'map',
   view: view
 });
