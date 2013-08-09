5811d9412eae8c56a74a89c3add251052a221cc8
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2013-08-09 19:55:17 +1200

    XYZ: added resolutions
    However, this made apparent my lack of understanding
    of the XYZ scheme.
    Plan is to implement the TileCache scheme for easier
    access to file-based caches.

--- examples/xyz-tiles.js
+++ examples/xyz-tiles.js
@@ -1,12 +1,27 @@
 goog.require('ol.Map');
+//goog.require('ol.interaction.DragRotateAndZoom');
 goog.require('ol.RendererHints');
 goog.require('ol.View2D');
 goog.require('ol.layer.TileLayer');
 goog.require('ol.source.DebugTileSource');
-goog.require('ol.source.OSM');
+//goog.require('ol.source.OSM');
 goog.require('ol.tilegrid.XYZ');
 goog.require('ol.source.ImageTileSource');
 
+function reverse(ar) {
+  var reversed = [];
+  for (var i = ar.length; --i >= 0;) {
+    reversed.push(ar[i]);
+  }
+  return reversed;
+}
+var resolutions = reverse([
+        3.527775872778806, 8.819439681947015,
+        17.63887936389403, 35.27775872778806,
+        88.19439681947017, 176.38879363894034,
+        352.7775872778807, 705.5551745557614,
+        1411.1103491115227, 2822.2206982230455
+    ]);
 function slashify(s) {
     var paddedThree = pad(s, 3);
     return s.match(/.{3}/g).join('/');
@@ -20,24 +35,42 @@ function pad(n, num) {
     return (s+n);
 }
 function createUrl(x, y, z) {
+    if (x < 0 || y < 0) {
+        return null;
+    }
     var url = 'http://proxy.zen.landcareresearch.co.nz/possums/cache/possum_2009_cache_nztm/portalgridnztm1/';
-    var zeros = '000';
-    var s = '0'+ z +'/'+ slashify(pad(x, 9)) +'/'+ slashify(pad(y, 9)) +'.png';
+    var s = '0'+ z +'/'+ 
+            pad(Math.floor(x/1000000),3) +'/'+ 
+            pad(Math.floor(x/1000) % 1000,3) +'/'+ 
+            pad(x % 1000,3) +'/'+
+            pad(Math.floor(y/1000000),3) +'/'+ 
+            pad(Math.floor(y/1000) % 1000,3) +'/'+ 
+            pad(y % 1000,3) +'.png';
+    //var s = '0'+ z +'/'+ slashify(pad(x, 9)) +'/'+ slashify(pad(y, 9)) +'.png';
     return url + s;
 }
 var view = new ol.View2D({
     center: [20000000,20001000],
-    zoom: 1,
+    zoom: 1,    
     maxZoom: 6
   });
+var source = new ol.source.ImageTileSource({
+      tileGrid: new ol.tilegrid.XYZ({
+        //origin: [1000000, 4700000.0000001],
+        //origin: [-ol.proj.EPSG3857.HALF_SIZE, ol.proj.EPSG3857.HALF_SIZE],
+        //origin: [0, 0],
+        maxZoom: 9,
+        resolutions: resolutions
+      }),
+      tileUrlFunction: function(opt) {
+        return createUrl(opt.x, opt.y, opt.z);
+      }
+    });
+//source.tileGrid.resolutions_ = resolutions; // HACK
 var map = new ol.Map({
   layers: [
     new ol.layer.TileLayer({
-      source: new ol.source.ImageTileSource({
-        tileUrlFunction: function(opt) {
-          return createUrl(opt.x, opt.y, opt.z);
-        }
-      })
+      source: source
     })
   ],
   renderer: ol.RendererHint.DOM,
