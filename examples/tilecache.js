84472837b9ad4cf1cf657f774326f457c1e2fde4
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2013-08-11 20:39:42 +1200

    UNSTABLE: Started on TileCache source and grid
    Still need to work out TileRange and the grid
    etc. This is just getting started.

---
+++ examples/tilecache.js
@@ -0,0 +1,93 @@
+goog.require('ol.Attribution');
+goog.require('ol.Map');
+goog.require('ol.Extent');
+goog.require('ol.RendererHints');
+goog.require('ol.View2D');
+goog.require('ol.layer.ImageLayer');
+goog.require('ol.proj');
+goog.require('ol.source.SingleImageWMS');
+//goog.require('ol.source.TiledVideoWMS');
+goog.require('ol.source.TiledWMS');
+
+var extent = [1000000, 4700000.0000001, 2200000, 6300000];
+var projection = ol.proj.configureProj4jsProjection({
+  code: 'EPSG:2193',
+  extent: extent,
+  units: 'm'
+});
+function reverse(ar) {
+  var reversed = [];
+  for (var i = ar.length; --i >= 0;) {
+    reversed.push(ar[i]);
+  }
+  return reversed;
+}
+var resolutions = reverse([
+        3.527775872778806, 8.819439681947015,
+        17.63887936389403, 35.27775872778806,
+        88.19439681947017, 176.38879363894034,
+        352.7775872778807, 705.5551745557614,
+        1411.1103491115227, 2822.2206982230455
+    ]);
+var layers = [
+  new ol.layer.TileLayer({
+    source: new ol.source.TiledWMS({
+      url: 'http://maps.scinfo.org.nz/basemaps/wms?',
+      params: {
+          'LAYERS': 'landscape_eco_painted_relief', 
+          'TILED': true,
+          'VERSION': '1.1.1'
+      },
+      extent: extent
+    })
+  }),
+  new ol.layer.TileLayer({
+    source: new ol.source.TiledWMS({
+      url: 'http://npm.landcareresearch.co.nz/possums/anim/videoproxy.php?',
+      params: {
+          'LAYERS': 'possum_videos_cache_nztm', 
+          'TILED': true,
+          'VERSION': '1.1.1'
+      },
+      tileGrid: new ol.tilegrid.TileGrid({
+        origin: [1000000, 4700000.0000001],
+        resolutions: resolutions
+      }),
+      extent: extent
+    })
+  })
+];
+var map = new ol.Map({
+  //renderer: ol.RendererHint.CANVAS,
+  renderer: ol.RendererHint.DOM,
+  layers: layers,
+  target: 'map',
+  view: new ol.View2D({
+    center: [1600000, 5500000],
+    projection: projection,
+    scales: [100000, 250000, 500000, 1000000, 2000000, 4000000, 8000000],
+    zoom: 2
+  })
+});
+function getVids() {
+  return document.getElementsByTagName('video');
+}
+function applyAll(vs, func) {
+  for (var i = -1, leni = vs.length; ++i < leni;){
+    vs[i][func]();
+  }
+}
+function setAll(vs, prop, value) {
+  for (var i = -1, leni = vs.length; ++i < leni;){
+    vs[i][prop] = value;
+  }
+}
+function playAll() {
+  applyAll(getVids(), 'play');
+}
+function pauseAll() {
+  applyAll(getVids(), 'pause');
+}
+function rewindAll() {
+  setAll(getVids(), 'currentTime', 0);
+}
