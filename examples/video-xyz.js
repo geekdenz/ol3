f47e8859c96c570026aa80b808545c83bbc6bf00
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2017-09-25 17:46:19 +1300

    Experiment with WMTS.

--- examples/video-xyz.js
+++ examples/video-xyz.js
@@ -2,14 +2,17 @@ goog.require('ol.Map');
 goog.require('ol.View');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.XYZ');
+goog.require('ol.format.WMTSCapabilities');
+goog.require('ol.source.OSM');
+goog.require('ol.source.WMTS');
 
-var extent = [1000000, 4700000.0000001, 2200000, 6300000];
-var projection = new ol.proj.Projection({
-  code: 'EPSG:2193',
-  extent: extent,
-  units: 'm'
-});
-ol.proj.addProjection(projection);
+//var extent = [1000000, 4700000.0000001, 2200000, 6300000];
+//var projection = new ol.proj.Projection({
+//  code: 'EPSG:2193',
+//  extent: extent,
+//  units: 'm'
+//});
+//ol.proj.addProjection(projection);
 // -20722863.033888973, -4961350.41607024
 Number.prototype.padLeft = function (n,str){
     return Array(n-String(this).length+1).join(str||'0')+this;
@@ -60,9 +63,9 @@ var resolutions = reverse([
         1411.1103491115227, 2822.2206982230455
     ]);
 var view = new ol.View({
-	center: [20000000,60000000],
+	//center: [20000000,60000000],
 	zoom: 0,
-	projection: projection,
+	//projection: projection,
 	maxZoom: 6
 });
 var source = new ol.source.XYZ({
@@ -79,25 +82,79 @@ var source = new ol.source.XYZ({
 		var y = -coord[2] - 1;
 		//coord[2] = y;
 		console.log('src',src,y)
-		imageTile.getImage().src = createVideoUrl(coord);
+		//imageTile.getImage().src = createVideoUrl(coord);
 	},
 	origin: [1000000, 4700000.0000001]
 });
-var map = new ol.Map({
-	target: 'map',
-	//renderer: ol.RendererHint.DOM, // doesn't work
-	layers: [
-		/*
-	new ol.layer.Tile({
-	  source: new ol.source.XYZ({
-		url: 'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png' +
-			'?apikey=0e6fc415256d4fbb9b5166a718591d71'
-	  })
-	})
-	*/
-		new ol.layer.Tile({
-			source: source
-		})
-	],
-	view: view
+//var parser = new ol.format.WMTSCapabilities();
+//var url = 'https://openlayers.org/en/v4.3.3/examples/data/WMTSCapabilities.xml';
+//// http://maps.scinfo.org.nz/cached/wmts?SERVICE=wmts&REQUEST=getcapabilities&VERSION=1.0.0
+//fetch(url).then(function(response) {
+//	return response.text();
+//}).then(function(text) {
+//	var result = parser.read(text);
+//	var options = ol.source.WMTS.optionsFromCapabilities(result, {
+//		layer: 'layer-7328',
+//		matrixSet: 'EPSG:3857'
+//	});
+//	var layers = [
+//			/*
+//	new ol.layer.Tile({
+//	  source: new ol.source.XYZ({
+//		url: 'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png' +
+//			'?apikey=0e6fc415256d4fbb9b5166a718591d71'
+//	  })
+//	})
+//	*/
+//			/*new ol.layer.Tile({
+//				source: source
+//			}),*/new ol.layer.Tile({
+//              source: new ol.source.OSM(),
+//              opacity: 0.7
+//            }), new ol.layer.Tile({
+//              opacity: 1,
+//              source: new ol.source.WMTS(options)
+//            })
+//		];
+//	var map = new ol.Map({
+//		target: 'map',
+//		//renderer: ol.RendererHint.DOM, // doesn't work
+//		layers: layers//,
+//		//view: view
+//	});
+//});
+
+
+var parser = new ol.format.WMTSCapabilities();
+var map;
+view = new ol.View({
+			center: [19412406.33, -5050500.21],
+			zoom: 5
+		});
+
+fetch('https://openlayers.org/en/v4.3.3/examples/data/WMTSCapabilities.xml').then(function(response) {
+	return response.text();
+}).then(function(text) {
+	var result = parser.read(text);
+	var options = ol.source.WMTS.optionsFromCapabilities(result, {
+		layer: 'layer-7328',
+		matrixSet: 'EPSG:3857'
+	});
+
+	map = new ol.Map({
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM(),
+				opacity: 0.7
+			})/*,
+			new ol.layer.Tile({
+				source: source
+			})*/
+		],
+		target: 'map',
+		view: view /*new ol.View({
+			center: [19412406.33, -5050500.21],
+			zoom: 5
+		})*/
+	});
 });
