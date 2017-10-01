0f3f68c86c6448484aac3f5b82ebe155dea450e0
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2017-10-01 19:50:54 +1300

    Experiment with synchronization.

--- examples/video-xyz.js
+++ examples/video-xyz.js
@@ -54,17 +54,19 @@ ol.VideoTile = function(tileCoord, state, src, crossOrigin, tileLoadFunction) {
 
 };
 ol.inherits(ol.VideoTile, ol.ImageTile);
-var currentTime = 0;
-var videos = [];
+/*
 function reset() {
 	videos.map((v) => v.currentTime = 1/6);
 }
+*/
 ol.VideoTile.prototype.handleImageLoad_ = function() {
   if (this.image_.videoWidth && this.image_.videoHeight) {
 	this.image_.width = this.image_.videoWidth;
 	this.image_.height = this.image_.videoHeight;
     this.state = ol.TileState.LOADED;
-	  reset();
+	  //this.image_.play();
+	ol.events.listen(this.image_, 'timeupdate', this.timeUpdated, this);
+	  //reset();
   } else {
     this.state = ol.TileState.EMPTY;
   }
@@ -79,12 +81,57 @@ ol.VideoTile.prototype.load = function() {
     this.imageListenerKeys_ = [
       ol.events.listenOnce(this.image_, ol.events.EventType.ERROR,
           this.handleImageError_, this),
+	//ol.events.listen(this.image_, 'progress', this.progress, this),
+	//ol.events.listenOnce(this.image_, 'loadeddata', this.loadedData, this),
+	//ol.events.listen(this.image_, 'timeupdate', this.timeUpdated, this),
       ol.events.listenOnce(this.image_, 'canplay',
       //ol.events.listenOnce(this.image_, ol.events.EventType.LOAD,
           this.handleImageLoad_, this)
     ];
     this.tileLoadFunction_(this, this.src_);
   }
+};
+ol.VideoTile.prototype.someLoaded = function(event) {
+	var seekable = this.image_.seekable;
+	var len = seekable && seekable.length;
+	//console.log('synching?');
+	if (!seekable) {
+		return;
+	}
+	var layer = this.layer;
+	var time = layer && layer.currentTime % this.image_.duration || 0;
+	if (layer && layer.currentDate) {
+		time += ((new Date()).getTime() - layer.currentDate) / 1000; // account for passed in code until now
+	}
+	for (var i = 0; i < len; ++i) {
+		if (seekable.start(i) <= time && time <= seekable.end(i)) {
+			this.image_.currentTime = time;
+			//console.log('synced', this.image_.currentTime);
+			return;
+		}
+	}
+};
+ol.VideoTile.prototype.timeUpdated = function(event) {
+	//console.log('timeupdate');
+	let video = this.getImage();
+	var currentTime = video.currentTime;
+	var videos = this.layer && this.layer.videos;
+	if (videos && videos.length) {
+		let duration = video.duration || 0;
+		let latestTime = videos.map((v) => v.currentTime).reduce((a, time) => time && Math.max(a, time % duration) || currentTime, currentTime);
+		if (latestTime < duration && latestTime > currentTime + 1/30) { // assume close enough HACK
+			//videos.filter((v) => v !== video).map((v) => v.currentTime = latestTime);
+			video.currentTime = latestTime;
+		} else if (latestTime === 0 && duration > 0) {
+			video.currentTime = 0; // need to reset
+		}
+	}
+};
+ol.VideoTile.prototype.progress = function(event) {
+	return this.someLoaded(event, false);
+};
+ol.VideoTile.prototype.loadedData = function(event) {
+	return this.someLoaded(event, false);
 };
 ol.source.VideoXYZ = function(opt_options) {
   var options = opt_options || {};
@@ -154,11 +201,14 @@ ol.layer.VideoTile = function(opt_options) {
    * @type {ol.LayerType}
    */
   this.type = ol.LayerType.TILE;
-	this.paused = false;
 
+	this.currentTime = 0;
 	//var videos = [];
+	this.videos = [];
 	//var highestTime = 0;
 	//var paused = this.isPaused();
+	var that = this;
+	this.timePerFrame = 1/60;
 	this.on('postcompose', function(event) {
 		var frameState = event.frameState;
 		var source = this.getSource();
@@ -167,7 +217,7 @@ ol.layer.VideoTile = function(opt_options) {
 		var ctx = event.context;
 		var tileSize = source.tileGrid.getTileSize();
 		var halfSize = tileSize / 2;
-		videos.length = 0;
+		that.videos.length = 0;
 		ctx.save();
 		ctx.scale(frameState.pixelRatio, frameState.pixelRatio);
 		tileGrid.forEachTileCoord(extent, view.getZoom(), function(tileCoord) {
@@ -176,9 +226,11 @@ ol.layer.VideoTile = function(opt_options) {
 			var topLeft = ol.coordinate.add(middlePixel, [-halfSize, -halfSize]);
 			var tile = source.getTile(tileCoord[0], tileCoord[1], tileCoord[2], 1, proj2193);
 			var video = tile.getImage();
-			currentTime = video.currentTime;
+			//that.currentTime = Math.max(video.currentTime, that.currentTime);
+			//that.currentDate = (new Date()).getTime();
 			//video.currentTime += 1000/30;
-			videos.push(video);
+			that.videos.push(video);
+			tile.layer = that;
 			//highestTime = Math.max(video.currentTime, highestTime);
 			ctx.drawImage(video, topLeft[0], topLeft[1]);
 		});
@@ -316,24 +368,32 @@ videoLayer.on('postcompose', function(event) {
 */
 var timeSlider = document.getElementById('time');
 var time = 0;
+var timePerFrame = 1/60;
+var currentDate = new Date();
 var f = function() {
+	var newDate = new Date();
+	timePerFrame = (newDate.getTime() - currentDate.getTime()) / 1000;
+	currentDate = newDate;
+	videoLayer.timePerFrame = timePerFrame;
 	map.render();
-	//requestAnimationFrame(f);
-	var index = videos.length - 1;
+	requestAnimationFrame(f);
+	//var index = videos.length - 1;
 	//time = videos[index] && videos[index].currentTime || 0;
-	time += 1/30;
-	time %= 30;
+	//time += 1/30;
+	//time %= 30;
 	//console.log(time);
-	timeSlider.value = time;
+	//timeSlider.value = time;
 };
-setInterval(f, 1000/30);
 /*
+setInterval(f, 1000/30);
+*/
 f();
+/*
 map.on('moveend', function() {
 	//videos.map((v) => v.currentTime = videos[videos.length - 1].currentTime);
 });
 */
-document.getElementById('reset').addEventListener('click', reset);
-document.getElementById('pause').addEventListener('click', () => videos.map((v) => v.pause()));
-document.getElementById('start').addEventListener('click', () => videos.map((v) => v.play()));
+//document.getElementById('reset').addEventListener('click', reset);
+//document.getElementById('pause').addEventListener('click', () => videos.map((v) => v.pause()));
+//document.getElementById('start').addEventListener('click', () => videos.map((v) => v.play()));
 
